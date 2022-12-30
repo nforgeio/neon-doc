2f1e284b30ca87771bd3bb2131e0093405d8c40f
Marcus Bowyer <marcus@euarctos.com>
2022-12-29 16:34:05 -0800

    change branch to master

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/nforgeio/documentation/edit/main/',
+            'https://github.com/nforgeio/documentation/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/nforgeio/documentation/edit/main/',
+            'https://github.com/nforgeio/documentation/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
