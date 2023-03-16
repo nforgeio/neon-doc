b6d2a2e3e5c727932acc9eec8d438a15c7b8547a
Marcus Bowyer <marcus@euarctos.com>
2023-03-15 17:30:50 -0700

    use postcss and minify css

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
     tailwindcss: {},
+
     autoprefixer: {},
+    cssnano: process.env.NODE_ENV === "production" ? {} : false,
   },
-}
+};
