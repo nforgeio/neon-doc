e54f97c2aba902b5ada1371960cd2a9557c11160
Marcus Bowyer <marcus@euarctos.com>
2023-04-14 03:43:36 -0700

    nav  styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./docs/**/*.mdx"], // my markdown stuff is in ../docs, not /src
-  darkMode: ['class', '[data-theme="dark"]'], // hooks into docusaurus' dark mode
+  darkMode: ["class", '[data-theme="dark"]'], // hooks into docusaurus' dark mode
   theme: {
     extend: {},
   },
   plugins: [],
-  corePlugins: { preflight: false }
-}
+  corePlugins: { preflight: false },
+};
