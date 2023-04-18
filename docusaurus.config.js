0a46fc834a136bde1c30934172120c6661dde6d2
Marcus Bowyer <marcus@euarctos.com>
2023-04-17 21:15:25 -0700

    algolia

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,35 @@ const config = {
             "neon, neonforge, neonkube, neondesktop, neon-desktop, neon-cli, kubernetes, documentation",
         },
       ],
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'RYA07XNW6G',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '2bbb75bbc651f3321c92bb8f239f88af',
+  
+        indexName: 'neonforge',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        // externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        // replaceSearchResultPathname: {
+        //   from: '/docs/', // or as RegExp: /\/docs\//
+        //   to: '/',
+        // },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
       colorMode: {
         defaultMode: "dark",
         disableSwitch: true,
