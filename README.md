73fd811f760153a23684b9a99a2085bc33882095
Jeff Lill <jeff@lilltek.com>
2024-03-10 14:03:47 -0700

    [buildenv.cmd]: configure NVM and install the required version of Node.js

--- README.md
+++ README.md
@@ -1,32 +1,14 @@
-# Documentation
+# Developer Setup
 
-NeonFORGE Documentation 
+1. Run `~\.buildenv.cmd` as administrator, which:
 
-# Website
+   * Configures the `ND_ROOT` environment variable to reference the **documentation** 
+	 git repo folder.
 
-This website is built using [Docusaurus 3](https://docusaurus.io/), a modern static website generator.
+   * Checks for **nvm** installation and executes `~\toolbin\nvm-setup.exe` when
+	 that's not already installed.  Answer YES if the installer asks to manage
+	 any existing Node.js installations.
 
+   * Sets **ND_NODEJS_VERSION=20.11.1**, specifying the Node.js version to use for bulding 
+	 documentartion, and then uses NVM to install that version of node.
 
-## Local Development
-
-### Install yarn
-```bash
-npm install --global yarn
-```
-
-### Add npm to the path
-
-```
-C:\Users\<User>\AppData\Roaming\npm
-```
-
-### To run the Dev server
-```bash
-yarn run start
-```
-
-This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
-
-### Helpers
-
-- [Live mermaid editor](https://mermaid.live/edit)
---
+++ buildenv.cmd
@@ -0,0 +1,95 @@
+@echo off
+REM Configures the environment variables required to build NEONDOCS projects.
+REM 
+REM 	buildenv [ <source folder> ]
+REM
+REM Note that <source folder> defaults to the folder holding this
+REM batch file.
+REM
+REM This must be [RUN AS ADMINISTRATOR].
+
+echo ============================================
+echo * NEONDOCS Build Environment Configurator  *
+echo ============================================
+
+REM Default ND_ROOT to the folder holding this batch file after stripping
+REM off the trailing backslash.
+
+set ND_ROOT=%~dp0 
+set ND_ROOT=%ND_ROOT:~0,-2%
+
+if not [%1]==[] set ND_ROOT=%1
+
+if exist %ND_ROOT%\.neon-docs goto goodPath
+echo The [%ND_ROOT%\.neon-docs] file does not exist.  Please pass the path
+echo to the [neon-docs] repo folder.
+goto done
+
+:goodPath 
+
+echo.
+echo Configuring...
+echo.
+
+REM Set NF_REPOS to the parent directory holding the NEONFORGE repositories.
+
+pushd "%NF_ROOT%\.."
+set NF_REPOS=%cd%
+popd
+
+REM Set other environment variables.
+
+set ND_NODEJS_VERSION=20.11.1
+
+REM Persist the environment variables.
+
+setx ND_ROOT "%ND_ROOT%" /M                   > nul
+setx ND_NODE_VERSION "%ND_NODEJS_VERSION%" /M > nul
+
+REM Check whether NVM (Node Version Manager) is installed and start its
+REM installed when it's not present.
+
+where NVM > nul 2> nul
+
+if "%ERRORLEVEL%" == "0" goto nvmOK
+
+echo.
+echo NVM (Node Version Manager) is required and needs to be installed.
+echo.
+set /P "INSTALL_NVM=Do you want to install NVM now? (y/n): "
+
+if "%INSTALL_NVM%"=="y" (
+    set INSTALL_NVM=1
+) else if "%INSTALL_NVM%"=="Y" (
+    set INSTALL_NVM=1
+) else if "%INSTALL_NVM%"=="n" (
+    set INSTALL_NVM=0
+) else if "%INSTALL_NVM%"=="N" (
+    set INSTALL_NVM=0
+) else (
+    echo.
+    echo "*** ERROR: You must answer with: Y or N."
+    echo.
+    goto justMockPrompt
+)
+
+if "%INSTALL_NVM%"=="1" (
+    "%ND_ROOT%\toolbin\nvm-setup.exe"
+) else (
+    echo.
+    echo "Build environment is NOT FULLY CONFIGURED."
+    exit 1
+)
+
+:nvmOK
+
+REM Install the required version of Node.js.
+
+nvm install %ND_NODEJS_VERSION%
+nvm use %ND_NODEJS_VERSION%
+
+:done
+echo.
+echo ============================================================================================
+echo * Be sure to close and reopen Visual Studio and any command windows to pick up the changes *
+echo ============================================================================================
