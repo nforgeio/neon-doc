99d52d496616f6283dfc31c4ffaeb0c0ead1f255
Jeff Lill <jeff@lilltek.com>
2024-03-13 12:56:30 -0700

    renamed [installation] folder to [getting-started] to match what;s displayed, added the [cluster-deployment] folder and standardize on the term "open source" over "opensource" or "open-source"

--- toolbin/neondoc-start.cmd
+++ toolbin/neondoc-start.cmd
@@ -2,9 +2,16 @@
 REM Starts the development server for the documentation at http://localhost:3000
 REM and then launches a browser that can be used for reviewing how the docs look.
 
+echo.
 echo ========================================================
 echo * Launching [neon-docs] development server and browser *
 echo ========================================================
+echo.
 
 cd %ND_ROOT%
 yarn start
+
+echo.
+echo Looks like the Docusaurus build quit or failed!
+echo.
+pause
