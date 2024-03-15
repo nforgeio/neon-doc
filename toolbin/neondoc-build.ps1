d141a03b484f3d668f22d82c829dbb98f318e9fd
Jeff Lill <jeff@lilltek.com>
2024-03-14 19:14:21 -0700

    copyright update

--- toolbin/neondoc-build.ps1
+++ toolbin/neondoc-build.ps1
@@ -2,7 +2,7 @@
 #------------------------------------------------------------------------------
 # FILE:         neondoc-build.ps1
 # CONTRIBUTOR:  Jeff Lill
-# COPYRIGHT:    Copyright (c) 2005-2021 by NEONFORGE LLC.  All rights reserved.
+# COPYRIGHT:    Copyright (c) 2005-2024 by NEONFORGE LLC.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
